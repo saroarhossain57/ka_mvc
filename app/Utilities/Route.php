5b7e1aa8717bf9679a1de96fa0258a32313ba299
Saroar Hossain <limonhossain57@gmail.com>
2022-09-05 20:42:53 +0600

    Routing working with controller

--- app/Utilities/Route.php
+++ app/Utilities/Route.php
@@ -35,9 +35,11 @@ class Route
 
         $callback = $this->getMatchedCallback($path, $method);
 
-        echo "<pre>";
-        echo var_dump($this->routes);
-        echo "<pre>";
+
+
+        if(is_callable($callback)){
+            echo $callback();
+        }
     }
 
     private function getMatchedCallback($path, $method){
@@ -46,16 +48,43 @@ class Route
         if(isset($this->routes[$method]) && !empty($this->routes[$method])){
 
             foreach ($this->routes[$method] as $key => $route){
+
+                $is_correct_route = false;
+
                 // Find the route
                 if($this->get_path_count($path) === $this->get_path_count($key)){
                     // Check if there any dynamic parameters in the path
-                    echo "<pre>";
-                    echo var_dump($path);
-                    echo var_dump($key);
-                    echo "<pre>";
-
+                    $is_correct_route = true;
                     // If there is dynamic parameter in the $key
+                    if(preg_match('/\{\w+\}/', $key)){
 
+                        $keys_array = explode('/', ltrim($key, '/'));
+                        $paths_array = explode('/', ltrim($path, '/'));
+                        $query_params = [];
+                        foreach ($keys_array as $index => $item){
+                            // Check if it is dynamic param
+                            if($item[0] == '{' && $item[strlen($item) - 1] == '}') {
+                                $query_params[substr($item, 1, -1)] = $paths_array[$index];
+                            } else {
+                                if($item !== $paths_array[$index]){
+                                    $is_correct_route = false;
+                                }
+                            }
+                        }
+
+                        if($is_correct_route){
+                            return [
+                                'route' => $route,
+                                'params' => $query_params
+                            ];
+                        }
+
+                    } else {
+                        // There is no dynamic parameter
+                        if($path === $key){
+                            return $route;
+                        }
+                    }
                 }
             }
 
